d0c4fe82597968d000015a1ad0c02ca6d4c6494f
Keithleen Ivana Ruíz Estrada <125908174+keithre19@users.noreply.github.com>
2024-07-19 12:33:40 -0600

    Revirtiendo cambios del main2

--- main2.py
+++ main2.py
@@ -1,8 +1,31 @@
-# Branching
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
 
-## Descripción
+@app.get('/')
+def read_root():
+    return {'message': 'Hello World'}
 
-Este proyecto tiene como objetivo enseñarles a utilizar de manera sencilla el branching, los code reviews y las pull request. 
+class Contraseña(BaseModel):
+    contraseña: str
 
-## Nombre de Arturo
-Arturo Monterroso - 201608006
+@app.post('/contraseña')
+async def registro(contraseña : Contraseña):
+    pw = contraseña.contraseña
+    if len(pw) > 8:
+        if any(c.islower() for c in pw):
+            if any(c.isupper() for c in pw):
+                if any(c.isdigit() for c in pw[1:-1]):
+                    if any(c in '!@#$' for c in pw[1:-1]):
+                        return {'mensaje': 'Contraseña aceptada'}
+                    else:       
+                        return {'mensaje': 'La contraseña debe contener al menos un caracter especial'}
+                else:
+                    return {'mensaje': 'La contraseña debe contener al menos un número'}
+            else:
+                return {'mensaje': 'La contraseña debe contener al menos una letra mayúscula'}
+        else:
+            return {'mensaje': 'La contraseña debe contener al menos una letra minúscula'}
+    else:
+        return {'mensaje': 'El tamaño de la contraseña debe ser mayor a 8 caracteres'}
+
