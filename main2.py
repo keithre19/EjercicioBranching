b30dcb2d89d6899abca4db66dd336b822d8ca536
Arturo Monterroso <110266239+arturomonterroso25@users.noreply.github.com>
2024-07-19 12:03:46 -0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,31 +1,8 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-app = FastAPI()
+# Branching
 
-@app.get('/')
-def read_root():
-    return {'message': 'Hello World'}
+## Descripción
 
-class Contraseña(BaseModel):
-    contraseña: str
+Este proyecto tiene como objetivo enseñarles a utilizar de manera sencilla el branching, los code reviews y las pull request. 
 
-@app.post('/contraseña')
-async def registro(contraseña : Contraseña):
-    pw = contraseña.contraseña
-    if len(pw) > 8:
-        if any(c.islower() for c in pw):
-            if any(c.isupper() for c in pw):
-                if any(c.isdigit() for c in pw[1:-1]):
-                    if any(c in '!@#$' for c in pw[1:-1]):
-                        return {'mensaje': 'Contraseña aceptada'}
-                    else:       
-                        return {'mensaje': 'La contraseña debe contener al menos un caracter especial'}
-                else:
-                    return {'mensaje': 'La contraseña debe contener al menos un número'}
-            else:
-                return {'mensaje': 'La contraseña debe contener al menos una letra mayúscula'}
-        else:
-            return {'mensaje': 'La contraseña debe contener al menos una letra minúscula'}
-    else:
-        return {'mensaje': 'El tamaño de la contraseña debe ser mayor a 8 caracteres'}
-
+## Nombre de Arturo
+Arturo Monterroso - 201608006
